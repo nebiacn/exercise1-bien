a9b8ee45aa19a5bc25130b71b07b07157a05fefc
Bien Gilzon Lara <bien.gilzon.c.lara@accenture.com>
2026-05-10 16:35:38 +0000

    80% done

--- webapp/controller/Exercise1.controller.js
+++ webapp/controller/Exercise1.controller.js
@@ -1,10 +1,18 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("exercise1.controller.Exercise1", {
         onInit() {
+            var oData = {   "SelectedCountry": "",
+                            "ListofCountries": [    { "Country": "England"  },
+                                                    { "Country": "Germany"  },
+                                                    { "Country": "USA"      },
+                                                    { "Country": "Philippines"  }   ]   }
+            var oModel  = new JSONModel(oData);
+            this.getView().setModel(oModel);
         }
     });
 });
